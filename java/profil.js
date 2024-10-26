7f08da7a04bd081cb3d8f0bfe8552470b4db6215
Terraluminiscentia <cumabacafypdoang@gmail.com>
2024-10-27 00:22:24 +0700

    Update profil.js

--- java/profil.js
+++ java/profil.js
@@ -1,7 +1,37 @@
-const menuIcon = document.querySelector('.menu-icon');
+ const menuIcon = document.querySelector('.menu-icon');
   const navLinks = document.querySelector('.nav-links');
-
+  
   menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show'); // Toggles the menu to show/hide
     menuIcon.classList.toggle('change'); // Toggles the 3-strip to "X"
+  });
+
+
+  function showPopup() {
+    document.getElementById("warningPopup").style.display = "flex";
+  }
+  
+  function closePopup() {
+    document.getElementById("warningPopup").style.display = "none";
+  }
+  
+  // Disable right-click
+  document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    showPopup();
+  });
+  
+  // Disable F12 and Ctrl+U
+  document.addEventListener("keydown", function (event) {
+    // Detect F12
+    if (event.key === "F12") {
+      event.preventDefault();
+      showPopup();
+    }
+  
+    // Detect Ctrl+U
+    if (event.ctrlKey && event.key === "u") {
+      event.preventDefault();
+      showPopup();
+    }
   });
